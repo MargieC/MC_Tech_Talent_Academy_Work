7697adfa9ad4a795681aab5f896e1eae91fafb45
MargieC <margarita.chakalova@gmail.com>
2021-07-05 16:39:18 +0100

    Update 20210705_MCTechTalent_Homework_Task2.py

--- 20210705_MCTechTalent_Homework_Task2.py
+++ 20210705_MCTechTalent_Homework_Task2.py
@@ -1,8 +1,8 @@
 #Task 2
 import random
-joke_1 = "Haha blah blah1"
-joke_2 = "Haha blah blah2"
-joke_3 = "Haha blah blah3"
+joke_1 = "Q: Why did the crab never share? A: Because he's shellfish"
+joke_2 = "Q: What was the dogs favorite type of homework to do? A: A lab report"
+joke_3 = "Q: What do you get if you cross an angry sheep and a moody cow? A: An animal that's in a baaaaaaaaad mooooooooooood."
 favourite_number = int(input("Hello, what is your favourite number between 1 and a 100? "))
 if favourite_number >= 0 and favourite_number <34:
     print(joke_1)
