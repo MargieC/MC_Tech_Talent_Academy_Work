bb5d59b0a13bfe36c14f3510dd0366da7e662cb0
MargieC <margarita.chakalova@gmail.com>
2021-07-05 16:25:36 +0100

    Update 20210705_MCTechTalent_Homework_Task5.py

--- 20210705_MCTechTalent_Homework_Task5.py
+++ 20210705_MCTechTalent_Homework_Task5.py
@@ -1,11 +1,17 @@
 #Task 5
 number_1 =int(input("Please enter the first number: "))
 number_2 =int(input("Please enter the second number: "))
-operator = input("Enter an operation (/ + - * ^2): ")
+operator = input("Enter an operation (/ + - * **): ")
 
 if operator == "/":
     print("Result: " + str(number_1/number_2))
 elif operator == "*":
     print("Result: " + str(number_1*number_2))
+elif operator == "+":
+    print("Result: " + str(number_1+number_2))
+elif operator == "-":
+    print("Result: " + str(number_1-number_2))
+elif operator == "**":
+    print("Result: " + str(number_1**number_2))
 else:
     print("Unknown operator " + operator)
